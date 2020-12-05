11b6bcaef99cb8d0889fe5b35c56a00d5f0c495e
Boozle <42422492+Boozle812@users.noreply.github.com>
2020-12-05 14:06:35 -0600

    added the main code file
    defines variables but not much else for now

---
+++ chronomod.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // runs once upon startup
+
+  // test LEDs for visual feedback (temporary)
+  int ledGreen = 0;
+  int ledBlue = 1;
+  int ledYellow = 2;
+  
+  // three potentiometers. analogRead returns values between 0 and 1023
+  int mainPotentiometer = 14;
+  int volumePotentiometer = 15;
+  int brightnessPotentiometer = 16;
+}
+
+void loop() {
+  // runs once every clock cycle
+
+}
